6631359532e113d002a0acd74ebd04879040f0d6
astha-2004-code <231220017@nitdelhi.ac.in>
2025-07-29 12:00:27 +0530

    Created Node Server

---
+++ controllers/testController.js
@@ -0,0 +1,9 @@
+export const testController = (req, res) => {
+	res.status(200).send({
+		message: "test route",
+		success: true,
+	});
+};
+
+
+
---
+++ routes/testRoutes.js
@@ -0,0 +1,7 @@
+const express = require('express')
+
+//router object
+const router = express.Router()
+
+//routes
+router.get('/test',)
