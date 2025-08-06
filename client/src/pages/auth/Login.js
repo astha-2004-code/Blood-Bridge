77c11591f313985c25edcd6dd22595b58c6761bc
astha-2004-code <231220017@nitdelhi.ac.in>
2025-08-06 23:32:53 +0530

    REACT

---
+++ client/src/pages/auth/Login.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import InputType from '../../components/shared/Form/InputType';
+
+const Login = () => {
+  return (
+    <>
+      <div className="row">
+        <div className="col-md-8 form-banner">
+          <img src="./assets/images/banner1.jpg" alt="loginImage" className="img-fluid" />
+        </div>
+        <div className="col-md-4 form-container">
+          <form>
+					  <InputType labelText={'EMail' } labelFor={'forEMail'} inputType={'email'} name={'email'}/>
+            <button type="submit" className="btn btn-primary">
+              Submit
+            </button>
+          </form>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Login;
