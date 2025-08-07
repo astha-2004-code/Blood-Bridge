ed5b8be7f7e451352d92f5d1b24e7d7c1b463e83
astha-2004-code <231220017@nitdelhi.ac.in>
2025-08-08 03:51:44 +0530

    Public and Private route created

--- client/src/App.js
+++ client/src/App.js
@@ -5,15 +5,31 @@ import Login from './pages/auth/Login.js';
 import Register from './pages/auth/Register';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import ProtectedRoute from './components/Routes/ProtectedRoute.js';
+import PublicRoute from './components/Routes/PublicRoute.js';
 
 function App() {
   return (
     <>
       <ToastContainer/>
       <Routes>
-        <Route path='/' element={<HomePage />} />
-        <Route path='/login' element={<Login />} />
-        <Route path='/register' element={<Register />} />
+        <Route path='/' element={
+          <ProtectedRoute>
+            <HomePage />
+          </ProtectedRoute>
+        
+        } />
+        <Route path='/login' element={
+          <PublicRoute>
+            <Login />
+          </PublicRoute>
+        } />
+        <Route path='/register' element={
+          <ProtectedRoute>
+            <Register />
+          </ProtectedRoute>
+        
+        } />
       </Routes>
     </>
   )
--- client/src/redux/features/auth/authSlice.js
+++ client/src/redux/features/auth/authSlice.js
@@ -1,58 +1,63 @@
-import { createSlice } from '@reduxjs/toolkit';
-import { userLogin, userRegister } from './authActions';
+import { createSlice } from "@reduxjs/toolkit";
+import { getCurrentUser, userLogin, userRegister } from "./authActions";
 
-const token = localStorage.getItem('token') ? localStorage.getItem('token') : null;
+const token = localStorage.getItem("token")
+  ? localStorage.getItem("token")
+  : null;
 
 const initialState = {
-    loading: false,
-    user: null,
-    token,
-    error: null,
+  loading: false,
+  user: null,
+  token,
+  error: null,
 };
 
 const authSlice = createSlice({
-    name: 'auth',
-    initialState,
-    reducers: {
-        logout: (state) => {
-            state.user = null;
-            state.token = null;
-            localStorage.removeItem('token');
-        }
-    },
-    extraReducers: (builder) => {
-        // Login
-        builder.addCase(userLogin.pending, (state) => {
-            state.loading = true;
-            state.error = null;
-        });
-        builder.addCase(userLogin.fulfilled, (state, { payload }) => {
-            state.loading = false;
-            state.user = payload.user;
-            state.token = payload.token;
-            localStorage.setItem('token', payload.token); // Store token
-        });
-        builder.addCase(userLogin.rejected, (state, { payload }) => {
-            state.loading = false;
-            state.error = payload;
-        });
-
-        // Register
-        // REGISTER user
-                builder.addCase(userRegister.pending, (state) => {
-                state.loading = true;
-                state.error = null;
-                });
-                builder.addCase(userRegister.fulfilled, (state, { payload }) => {
-                state.loading = false;
-                 state.user = payload.user;
-                });
-                 builder.addCase(userRegister.rejected, (state, { payload }) => {
-                 state.loading = false;
-                 state.error = payload;
-          });
-       }
+  name: "auth",
+  initialState: initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    // login user
+    builder.addCase(userLogin.pending, (state) => {
+      state.loading = true;
+      state.error = null;
+    });
+    builder.addCase(userLogin.fulfilled, (state, { payload }) => {
+      state.loading = false;
+      state.user = payload.user;
+      state.token = payload.token;
+    });
+    builder.addCase(userLogin.rejected, (state, { payload }) => {
+      state.loading = false;
+      state.error = payload;
+    });
+    // REGISTER user
+    builder.addCase(userRegister.pending, (state) => {
+      state.loading = true;
+      state.error = null;
+    });
+    builder.addCase(userRegister.fulfilled, (state, { payload }) => {
+      state.loading = false;
+      state.user = payload.user;
+    });
+    builder.addCase(userRegister.rejected, (state, { payload }) => {
+      state.loading = false;
+      state.error = payload;
+    });
+    // CURRENT user
+    builder.addCase(getCurrentUser.pending, (state) => {
+      state.loading = true;
+      state.error = null;
+    });
+    builder.addCase(getCurrentUser.fulfilled, (state, { payload }) => {
+      state.loading = false;
+      state.user = payload.user;
+    });
+    builder.addCase(getCurrentUser.rejected, (state, { payload }) => {
+      state.loading = false;
+      state.error = payload;
+    });
+  },
 });
 
-export const { logout } = authSlice.actions;
 export default authSlice;
