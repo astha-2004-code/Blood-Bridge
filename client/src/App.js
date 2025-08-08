bf10b04aee8bf69f68841689d4e598ba21e7bd70
astha-2004-code <231220017@nitdelhi.ac.in>
2025-08-08 12:13:09 +0530

    Donars record and refactor

--- client/src/App.js
+++ client/src/App.js
@@ -1,38 +1,52 @@
-import {Routes,Route} from 'react-router-dom'
-import HomePage from './pages/HomePage';
-import Login from './pages/auth/Login.js';
-
-import Register from './pages/auth/Register';
-import { ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
-import ProtectedRoute from './components/Routes/ProtectedRoute.js';
-import PublicRoute from './components/Routes/PublicRoute.js';
-
+import { Routes, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import Login from "./pages/auth/Login";
+import Register from "./pages/auth/Register";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import ProtectedRoute from "./components/Routes/ProtectedRoute";
+import PublicRoute from "./components/Routes/PublicRoute";
+import Donar from "./pages/Dashboard/Donar";
 function App() {
   return (
     <>
-      <ToastContainer/>
+      <ToastContainer />
       <Routes>
-        <Route path='/' element={
-          <ProtectedRoute>
-            <HomePage />
-          </ProtectedRoute>
-        
-        } />
-        <Route path='/login' element={
-          <PublicRoute>
-            <Login />
-          </PublicRoute>
-        } />
-        <Route path='/register' element={
-          <ProtectedRoute>
-            <Register />
-          </ProtectedRoute>
-        
-        } />
+        <Route
+          path="/donar"
+          element={
+            <ProtectedRoute>
+              <Donar />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/"
+          element={
+            <ProtectedRoute>
+              <HomePage />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/login"
+          element={
+            <PublicRoute>
+              <Login />
+            </PublicRoute>
+          }
+        />
+        <Route
+          path="/register"
+          element={
+            <PublicRoute>
+              <Register />
+            </PublicRoute>
+          }
+        />
       </Routes>
     </>
-  )
+  );
 }
 
 export default App;
--- routes/inventoryRoutes.js
+++ routes/inventoryRoutes.js
@@ -1,15 +1,21 @@
-const express = require('express')
-const authMiddelware = require('../middlewares/authMiddelware')
-const { createInventoryController, getInventoryController } = require('../controllers/inventoryController')
+const express = require("express");
+const authMiddelware = require("../middlewares/authMiddelware");
+const {
+  createInventoryController,
+  getInventoryController,
+  getDonarsController,
+} = require("../controllers/inventoryController");
 
-const router = express.Router()
+const router = express.Router();
 
 //routes
-//ADD INVENTORY||POST
-router.post('/create-inventory', authMiddelware, createInventoryController);
+// ADD INVENTORY || POST
+router.post("/create-inventory", authMiddelware, createInventoryController);
 
 //GET ALL BLOOD RECORDS
-router.get('/get-inventory',authMiddelware,getInventoryController)
+router.get("/get-inventory", authMiddelware, getInventoryController);
 
+//GET DONAR RECORDS
+router.get("/get-donars", authMiddelware, getDonarsController);
 
 module.exports = router;
