e04cd10c6734f3f6f9325020190455ad669aa60d
astha-2004-code <231220017@nitdelhi.ac.in>
2025-08-08 08:12:38 +0530

    Layout and Logout action

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -1,12 +1,13 @@
 import React from "react";
 import { useSelector } from "react-redux";
 import Spinner from "../components/shared/Spinner";
+import Layout from "../components/shared/Layout/layeout";
 
 const HomePage = () => {
   const { loading, error } = useSelector((state) => state.auth);
 
   return (
-    <>
+    <Layout>
       {error && alert(error)}
       {loading ? (
         <Spinner />
@@ -15,7 +16,7 @@ const HomePage = () => {
           <h1>Home Page</h1>
         </>
       )}
-    </>
+    </Layout>
   );
 };
 
